cb7e61fd48244f55b8f7f3785966fd7c3f3112fa
M4YH3M7 <tengodzebisashvili6@gmail.com>
2025-11-17 23:21:19 +0400

    sanandr

--- list/lists/src/App.jsx
+++ list/lists/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+// import Fruits1 from "./component/Fruits1.jsx"
 
 
-
-function App() {
+export default function App() 
+{
+  const fruits = [
+    { name: "Apple", calories: 95 },
+    { name: "Banana", calories: 105 },
+    { name: "Cherry", calories: 50 },
+    { name: "Pineapple", calories: 452 },
+    { name: "Elderberry", calories: 73 },
+  ];
   
 
-  return (
-    <>
-    
-    </>
-  )
+  // const modified = fruits.map((fruit)=><li>{fruits.name} has {fruits.calories}</li>)
+const modified = fruits.map(fruit => (
+    <li>
+      {fruit.name} has {fruit.calories} calories
+    </li>
+  ));
+  return 
+  <ol>
+    {modified}
+  </ol>
 }
-
-export default App
